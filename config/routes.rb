578102b516154b7b4d2dad6137bd6b3e1e5bc31e
sissi.zorfi <qaconsulting@icloud.com>
2018-02-22 15:30:11 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-	
-resources :docs
-  root 'welcome#index'
+  devise_for :users
+	resources :docs
+	authenticated :user do
+		root "docs#index", as: "authenticated_root"
+	end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: "welcome#index"
 end
+
+
